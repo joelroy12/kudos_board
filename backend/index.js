d3dda4b705b7784b19e2cfa8f2842634b5364bc4
Joel Joseph Roy <joelroy@meta.com>
2025-06-18 11:09:46 -0700

    Attempt #1 in fixing deployment problems for backend by specifying port

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient()
-const PORT = 3000;
-const boardRoutes = require('./routes/boardRoutes')
-const cardRoutes = require('./routes/cardRoutes')
-const commentRoutes = require('./routes/commentRoutes');
+const prisma = new PrismaClient();
+const boardRoutes = require("./routes/boardRoutes");
+const cardRoutes = require("./routes/cardRoutes");
+const commentRoutes = require("./routes/commentRoutes");
+const port = process.env.PORT || 5433;
 
-app.use('./boards', boardRoutes)
-app.use('./cards', cardRoutes)
-app.use('./comments', commentRoutes)
-
+app.use("./boards", boardRoutes);
+app.use("./cards", cardRoutes);
+app.use("./comments", commentRoutes);
